680409716aaf6d990bd377efffe68b448d02cad5
tsukiyo10884 <tsukiyo10884@gmail.com>
2025-05-31 21:57:10 +0800

    fix css part 2

--- mai-tools/rating.js
+++ mai-tools/rating.js
@@ -8,7 +8,7 @@ function createRatingSection(title, songs) {
 function createSongCard(song) {
     const diffClass = song.difficulty.replace(" ", "-").toLowerCase();
     return `
-    <div class="song-card col-md-15 ${diffClass}" style="background-image: url('${song.image}')">
+    <div class="song-card ${diffClass}" style="background-image: url('${song.image}')">
         <div class="song-overlay"></div>
         <div class="block-song-name song-content text-shadow song-title f_10">${song.title}</div>
         <div class="block-inner-level song-content text-shadow f_10">${song.internalLevel == null ? '' : Number.parseFloat(song.internalLevel).toFixed(1)} | ${song.type.toUpperCase()}</div>
