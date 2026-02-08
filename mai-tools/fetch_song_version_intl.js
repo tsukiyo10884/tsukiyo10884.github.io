5d88dada9e52cc1b95e1e9fba9f48a9c63624e19
tsukiyo10884 <tsukiyo10884@gmail.com>
2026-02-08 16:20:51 +0800

    adjust function fetching intl version of songs

--- mai-tools/fetch_song_version_intl.js
+++ mai-tools/fetch_song_version_intl.js
@@ -1,21 +1,23 @@
 (async () => {
     const siteOrigin = new URL(document.currentScript.src).origin;
-    const versions = ["maimai", "maimai PLUS"].concat((await fetch(`${siteOrigin}/mai-tools/json/version.json`).then(res => res.json())).map(function (e) { return e.versionName }).slice(1));
+    const versions = ["maimai", "maimai PLUS"].concat(
+        (await fetch(`${siteOrigin}/mai-tools/json/version.json`)
+            .then(res => res.json()))
+            .map(function (e) {
+                if (!e.versionName.includes("~")) { return e.versionName }
+            }))
+        .filter(e => e !== undefined);
 
     const titleTypeToVersion = {};
 
     for (let i = 0; i < versions.length; i++) {
-        const res = await fetch(`https://maimaidx-eng.com/maimai-mobile/record/musicVersion/search/?version=${i}&diff=3`, {
+        const res = await fetch(`https://maimaidx-eng.com/maimai-mobile/record/musicVersion/search/?version=${i}&diff=2`, {
             credentials: 'include'
         });
         const text = await res.text();
         const doc = new DOMParser().parseFromString(text, 'text/html');
 
-        if (versions[i] !== doc.querySelector('.screw_block.m_15.f_15.p_s').textContent) {
-            continue;
-        }
-
-        const blocks = doc.querySelectorAll('.music_master_score_back.pointer.w_450.m_15.p_3.f_0');
+        const blocks = doc.querySelectorAll('.music_expert_score_back.pointer.w_450.m_15.p_3.f_0');
 
         blocks.forEach(block => {
             const title = block.querySelector('.music_name_block.t_l.f_13.break')?.textContent.trim() || "　";
